d5b4526cf961b3d87788029881a955fbdc58d45e
Aleksandar Damjanovic <codejanovic@zrdj.org>
2024-04-13 18:55:10 +0200

    reworked method of computing the roman numeral

--- pr1.a2/src/main/java/io/github/hsma/u3015487/task4/RomanNumerals.java
+++ pr1.a2/src/main/java/io/github/hsma/u3015487/task4/RomanNumerals.java
@@ -1,7 +1,5 @@
 package io.github.hsma.u3015487.task4;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -11,22 +9,24 @@ import java.util.Scanner;
 public class RomanNumerals {
 
     public static void main(String[] args) {
-        // Sortierte Map mit mapping von Zahl zu Römischer Zahl
-        // inklusive aller möglichen Subtraktionen
-        final Map<Integer, String> romanNumerals = new LinkedHashMap<>();
-        romanNumerals.put(1000, "M");
-        romanNumerals.put(900, "CM"); // mögliche Subtraktion
-        romanNumerals.put(500, "D");
-        romanNumerals.put(400, "CD"); // mögliche Subtraktion
-        romanNumerals.put(100, "C");
-        romanNumerals.put(90, "XC"); // mögliche Subtraktion
-        romanNumerals.put(50, "L");
-        romanNumerals.put(40, "XL"); // mögliche Subtraktion
-        romanNumerals.put(10, "X");
-        romanNumerals.put(9, "IX"); // mögliche Subtraktion
-        romanNumerals.put(5, "V");
-        romanNumerals.put(4, "IV"); // mögliche Subtraktion
-        romanNumerals.put(1, "I");
+        final int[] romanDecimals = new int[13];
+        final String[] romanNumerals = new String[13];
+
+        //Initialisieren des Arrays mit den notwendigen Kombinationen, abgeleitet aus den Anforderungen.
+        romanDecimals[0] = 1000; romanNumerals[0] = "M";
+        romanDecimals[1] = 900; romanNumerals[1] = "CM"; // mögliche Subtraktion
+        romanDecimals[2] = 500; romanNumerals[2] = "D";
+        romanDecimals[3] = 400; romanNumerals[3] = "CD"; // mögliche Subtraktion
+        romanDecimals[4] = 100; romanNumerals[4] = "C";
+        romanDecimals[5] = 90; romanNumerals[5] = "XC"; // mögliche Subtraktion
+        romanDecimals[6] = 50; romanNumerals[6] = "L";
+        romanDecimals[7] = 40; romanNumerals[7] = "XL"; // mögliche Subtraktion
+        romanDecimals[8] = 10; romanNumerals[8] = "X";
+        romanDecimals[9] = 9; romanNumerals[9] = "IX"; // mögliche Subtraktion
+        romanDecimals[10] = 5; romanNumerals[10] = "V";
+        romanDecimals[11] = 4; romanNumerals[11] = "IV"; // mögliche Subtraktion
+        romanDecimals[12] = 1; romanNumerals[12] = "I";
+
 
         try (final Scanner in = new Scanner(System.in)) {
             System.out.println("Bitte eine Ganzzahl zwischen 1 und 3999 eingeben:");
@@ -37,24 +37,7 @@ public class RomanNumerals {
                 throw new RuntimeException("Zahl musst zwischen 1 und 3999 liegen");
             }
 
-            // Ergebnis römische Zahl
-            final StringBuilder romanNumeral = new StringBuilder();
-            int reducingNumber = userNumber;
-
-            // Iteriert absteigend über die definierten Zahlen
-            for (final int number : romanNumerals.keySet()) {
-                final String romanLiteral = romanNumerals.get(number);
-                // Wenn die aktuelle Zahl der Zahl aus der Map entspricht, sind wir fertig
-                if (reducingNumber == number) {
-                    romanNumeral.append(romanLiteral);
-                    break;
-                }
-                // Subtrahieren solange es aufgeht
-                while (isPositive(reducingNumber - number)) {
-                    reducingNumber -= number;
-                    romanNumeral.append(romanLiteral);
-                }
-            }
+            final String romanNumeral = toRomanNumeral(userNumber, romanDecimals, romanNumerals);
 
             System.out.println("Römische Zahl: " + romanNumeral);
             System.out.println("Tschüss!");
@@ -62,12 +45,36 @@ public class RomanNumerals {
     }
 
     /**
-     * Ermittelt ob die übergebene Zahle positiv ist
+     * Ermittlung der römischen Zahl durch wiederholtes subtrahieren des nahestehendsten dezimalen Äquivalents mit Hilfe von Rekursion
      *
      * @param number
+     * @param decimals
+     * @param numerals
      * @return
      */
-    private static boolean isPositive(final int number) {
-        return number > 0;
+    private static String toRomanNumeral(final int number, final int[] decimals, final String[] numerals) {
+        final int floorIndex =  floorNumberIndex(number, decimals);
+        final int romanDecimal = decimals[floorIndex];
+        final String romanNumeral = numerals[floorIndex];
+
+        if ( number == romanDecimal) {
+            return romanNumeral;
+        }
+        return numerals[floorIndex] + toRomanNumeral((number-romanDecimal), decimals, numerals);
+    }
+
+    /**
+     * Ermittel den Index der nächsten Zahl, die kleiner oder gleich der übergebenen Zahl ist
+     * @param number
+     * @return array index
+     */
+    private static int floorNumberIndex(final int number, final int[] decimals) {
+        for (int i = 0; i < decimals.length; i++) {
+            final int decimal = decimals[i];
+            if (decimal <= number) {
+                return i;
+            }
+        }
+        throw new IllegalStateException("algo scheint nicht zu stimmen");
     }
 }
